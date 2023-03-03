1543a062a96fdf62be1f88e8a5cc31752b51d013
DimaLedentsov <63366388+DimaLedentsov@users.noreply.github.com>
2023-03-03 18:25:18 +0300

    ...

--- app/src/main/java/lab2/logic/methods/SecMethod.java
+++ app/src/main/java/lab2/logic/methods/SecMethod.java
@@ -4,6 +4,9 @@ import lab2.logic.Function;
 import lombok.Getter;
 import lombok.Setter;
 import static java.lang.Math.*;
+
+import java.util.ArrayList;
+import java.util.List;
 public class SecMethod {
     @Getter @Setter
     private Function function;
@@ -16,10 +19,21 @@ public class SecMethod {
     @Getter 
     double abs;
     double Fx;
+    @Getter int n;
     private double x,x_prev, x_next;
+    @Getter
+    private List<double[]> table = new ArrayList<>();
+
+    private double fXnext;
+    void save(){
+        table.add(new double[]{x_prev,x,x_next,fXnext,abs});
+    }
+    double f(double x){
+        return function.call(x);
+    }
     void solve(){
         x=0; x_prev=a; x_next=b;
-        int count = 0;
+        n = 1;
         while(abs(x-x_prev)>accuracy && abs(Fx)>accuracy){
             
         }
